c30672a3912644e07ee78b73e312aac2ae4c6ed0
vijay0221 <vijaymaroju2003@gmail.com>
2023-09-13 12:15:36 +0530

    added pages

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import Product from './pages/Product'
+import Pricing from './pages/Pricing'
+import Homepage from './pages/Homepage'
+import PageNotFount from "./pages/PageNotFount"
+import AppLayout from "./pages/AppLayout"
+import Login from "./pages/Login"
+
+
+function App() {
+  
+  return (
+   <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Homepage/>}/>
+      <Route path="product" element={<Product/>}/>
+      <Route path="pricing" element={<Pricing/>}/>
+      <Route path="login" element={<Login/>}/>
+      <Route path="app" element={<AppLayout/>}/>
+      <Route path="*" element={<PageNotFount/>}/>
+
+    </Routes>
+   </BrowserRouter>
+  )
+}
+
+export default App
---
+++ src/pages/PageNotFount.jsx
@@ -0,0 +1,12 @@
+import PageNav from "../components/PageNav"
+
+function PageNotFount() {
+  return (
+    <div>
+    <PageNav/>
+      Not found:
+    </div>
+  )
+}
+
+export default PageNotFount
